c8963a7a8ff91972b24af0f4fa2df2dc0c12b245
Bartosz T <bartosz@telenczuk.pl>
2015-07-05 01:12:38 +0200

    added plotting of multiple files

--- materials/07_scientific_workflows/plot_corr_coef.py
+++ materials/07_scientific_workflows/plot_corr_coef.py
@@ -5,15 +5,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 import argparse
+from itertools import cycle
 
 parser = argparse.ArgumentParser()
-parser.add_argument('corr_file')
+parser.add_argument('corr_file', nargs='+')
 args = parser.parse_args()
+bin_width = 0.02
 
-data = np.load(args.corr_file)
-corr_coefs = data['corr_coefs']
+colors = cycle(['r', 'b'])
+bins = np.arange(-1, 1, bin_width)
+for filename in args.corr_file:
 
-plt.hist(corr_coefs, 10)
+    data = np.load(filename)
+    corr_coefs = data['corr_coefs']
+
+    n, _ = np.histogram(corr_coefs,  bins)
+    plt.bar(bins[:-1], n, bin_width, fc=colors.next())
 
 plt.xlabel('correlation coefficient')
 plt.ylabel('number of pairs')
